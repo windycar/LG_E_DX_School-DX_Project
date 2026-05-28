ad6d2bee007fd0dc29250b24645d191e60fcb457
PKB <pargb0553@naver.com>
2026-05-28 17:03:18 +0900

    workspace.ver2_test

--- hello.py
+++ hello.py
@@ -3,9 +3,7 @@ print("hello world")
 print("hello world")
 #헬로 월드 출력
 print("hello world")
-<<<<<<< HEAD
-=======
+
+#헬로 월드 출력
 #헬로 월드 출력
 print("hello world")
->>>>>>> ce0a6e6df01ef0b673876bacac4f9836689eaa35
-#헬로 월드 출력
